e053c8a4b3f6a217b0eb64c9e5cf53870040984a
DikshaThakur1607 <dthakur2299@gmail.com>
2025-08-16 20:27:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 <script>
-  function toggleMenu() 
-  {
-    document.getElementById("navMenu").classList.toggle("active")
+let lastScroll = 0;
+const header = document.getElementById("site-header");
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+
+  if (currentScroll > lastScroll) {
+    // scrolling down -> hide header
+    header.style.top = "-120px";  // move header up
+  } else {
+    // scrolling up -> show header
+    header.style.top = "0";
   }
+
+  lastScroll = currentScroll;
+});
 </script>
+
+
